9ca3f04124bebf351f4080353b6a2bc8af97d045
Eason Sy <112996391+SyKaHo@users.noreply.github.com>
2024-07-22 20:25:18 +0800

    new

--- admin/js/schedule.js
+++ admin/js/schedule.js
@@ -1,3 +1,6 @@
 function openMap(location) {
     window.open('https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(location), '_blank');
 }
+function loadSchedule(page) {
+    window.location.href = page;
+}
