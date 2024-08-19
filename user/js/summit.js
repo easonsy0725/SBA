afa2baa7acaa88b25c4c761946840c3090105abd
Eason Sy <112996391+SyKaHo@users.noreply.github.com>
2024-08-19 17:36:44 +0800

    new

--- user/js/summit.js
+++ user/js/summit.js
@@ -1,3 +1,42 @@
+// change page
 function loadSchedule(page) {
   window.location.href = page;
 }
+
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the form element
+  var form = document.getElementById('summit-form');
+
+  // Add an event listener for the 'submit' event
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get the redirect URL from the data-redirect-url attribute
+    var redirectUrl = this.getAttribute('data-redirect-url');
+
+    // Perform any desired form processing or validation here
+
+    // Redirect the user to the specified URL
+    window.location.href = redirectUrl;
+
+    alert("submit successful!")
+  });
+});
+
+// function submitForm(event) {
+//   event.preventDefault(); // Prevent the default form submission behavior
+  
+//   // get the form element
+//   var form = event.target;
+  
+//   // get the redirect url from the data-redirect-url attribute
+//   var redirectUrl = form.getAttribute('data-redirect-url');
+  
+//   // perform any desired form processing or validation here
+  
+//   // redirect the user to the specified URL
+//   window.location.href = redirectUrl;
+
+//   alert("submit successful!")
+// }
